f53770b9692d2b7105b3ca5d828b2a436ac57e37
kchoest30 <kchoest@gmail.com>
2025-03-18 20:57:08 -0600

    Se incluye funcion de sorteo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-
+let listaNumerosSorteados = [];
 function agregarAmigo() {
     if (document.getElementById("amigo").value == "") {
         alert("Por favor, inserte un nombre.");
@@ -14,4 +14,27 @@ function agregarAmigo() {
         };
         document.getElementById("amigo").value = '';
     };
+}
+
+
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert("La lista esta vacia, incluye los participantes!")
+    } else {
+        for (y = 0; y < amigos.length; y++) {
+            if (listaNumerosSorteados.length < totalItems){
+                numeroSorteado = Math.floor(Math.random()*totalItems);
+                if (numeroSorteado == y || listaNumerosSorteados.includes(numeroSorteado)) {
+                    console.log(numeroSorteado + "Reintentar sorteo");
+                    return sortearAmigos(y);
+                } else {
+                    listaNumerosSorteados.push(numeroSorteado);
+                }
+            }
+        }
+        for (let i=0; i < amigos.length; i++){
+            document.getElementById("resultado").innerHTML += `<ul>${amigos[i]} regala a ${listaNumerosSorteados[i]}</ul>`;
+        }
+    }
+
 }
